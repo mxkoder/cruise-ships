5c1a819a72f226b880defd6c75080649935f1b4c
Agnes Beviz <agnes.beviz@gmail.com>
2022-06-24 22:27:36 +0100

    tests for setSail and setSail method added to prototype of Ship constructor

--- __tests__/Ships.test.js
+++ __tests__/Ships.test.js
@@ -10,30 +10,14 @@ describe('ship', () => {
 
       expect(ship.currentPort).toBe('Venice');
     });
-
   });
 
-
-
-/*
-    it('sets the name property - returns an object with the name property passed in as an argument', () => {
-      const pet = new Pet('Fido');
-      expect(pet.name).toEqual('Fido');
+  describe('setSail', () => {
+    it('changes the currentPort property of Ship to 0 when applied', () => {
+      const ship = new Ship('Venice');
+      ship.setSail();
+      
+      expect(ship.currentPort).toBeFalsy();
     });
+  });
 
-    it('has an age property with an initial value of 0', () => {
-      const pet = new Pet('Fido');
-      expect(pet.age).toEqual(0);
-    });
-
-    it('has a hunger property with an initial value of 0', () => {
-      const pet = new Pet('Fido');
-      expect(pet.hunger).toEqual(0);
-    });
-
-    it('has a fitness property with an initial value of 0', () => {
-      const pet = new Pet('Fido');
-      expect(pet.fitness).toEqual(10);
-    });
-
-  */
--- src/Ship.js
+++ src/Ship.js
@@ -2,4 +2,9 @@ function Ship (currentPort) {
   this.currentPort = currentPort;
 }
 
+Ship.prototype.setSail = function () {
+  this.currentPort = 0;
+}
+
+
 module.exports = Ship;
